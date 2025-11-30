08521f2195b5edd447bced53426d6ebed54edffb
kshafiq <khalid.shafiq99@gmail.com>
2025-11-30 01:05:50 -0500

    F1 added version 6.0

--- simple.cpp
+++ simple.cpp
@@ -1,4 +1,6 @@
 (1) simple.cpp was created in main branch. Nothing else added yet.
 (2) Updated simple.cpp was in main after F1 branch was created.
 (3) MAIN BRANCH ADDED LINE 3 AND VERSION 4.0
-(4) F1 BRANCH ADDED  LINE 3 AND VERSION 5.0
+(4) F1 BRANCH ADDED  LINE 4 AND VERSION 5.0
+(5) F1 BRANCH ADDED  LINE 5 AND VERSION 6.0
+
